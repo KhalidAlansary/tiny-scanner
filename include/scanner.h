77d8121718e5d71540b4969e3da12bb32ec98650
Khalid Alansary <khaled.ansary2003@outlook.com>
2024-12-03 15:55:04 +0200

    chore: add documentation for scanner.h

--- include/scanner.h
+++ include/scanner.h
@@ -5,38 +5,60 @@
 
 #define MAX_LEXEME_SIZE 100
 
+/**
+ * @brief Enumeration of possible token types.
+ */
 typedef enum {
-  ADD,
-  SUB,
-  MUL,
-  DIV,
-  EQ,
-  LT,
-  LPAREN,
-  RPAREN,
-  SEMI,
-  ASSIGN,
-  ID,
-  NUM,
-  ERROR,
-  IF,
-  THEN,
-  ELSE,
-  END,
-  REPEAT,
-  UNTIL,
-  READ,
-  WRITE,
-  END_OF_FILE,
+  ADD,        /**< Addition operator */
+  SUB,        /**< Subtraction operator */
+  MUL,        /**< Multiplication operator */
+  DIV,        /**< Division operator */
+  EQ,         /**< Equality operator */
+  LT,         /**< Less than operator */
+  LPAREN,     /**< Left parenthesis */
+  RPAREN,     /**< Right parenthesis */
+  SEMI,       /**< Semicolon */
+  ASSIGN,     /**< Assignment operator */
+  ID,         /**< Identifier */
+  NUM,        /**< Number */
+  ERROR,      /**< Error token */
+  IF,         /**< If keyword */
+  THEN,       /**< Then keyword */
+  ELSE,       /**< Else keyword */
+  END,        /**< End keyword */
+  REPEAT,     /**< Repeat keyword */
+  UNTIL,      /**< Until keyword */
+  READ,       /**< Read keyword */
+  WRITE,      /**< Write keyword */
+  END_OF_FILE /**< End of file */
 } TokenType;
 
+/**
+ * @brief Structure representing a token.
+ */
 typedef struct {
-  TokenType type;
-  char* lexeme;
+  TokenType type; /**< Type of the token */
+  char* lexeme;   /**< Lexeme of the token */
 } Token;
 
-typedef enum { START, INNUM, INID, INASSIGN, INCOMMENT, DONE } State;
+/**
+ * @brief Enumeration of possible states in the scanner.
+ */
+typedef enum {
+  START,     /**< Start state */
+  INNUM,     /**< Inside a number */
+  INID,      /**< Inside an identifier */
+  INASSIGN,  /**< Inside an assignment */
+  INCOMMENT, /**< Inside a comment */
+  DONE       /**< Done state */
+} State;
 
+/**
+ * @brief Get the next token from the file.
+ *
+ * @param file The file to read from.
+ * @return Token The next token.
+ */
 Token getNextToken(FILE* file);
 
 #endif
